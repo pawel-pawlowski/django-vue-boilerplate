0573227348218c067f51475b0753671217cd00da
Paweł Pawłowski <pawel.pawlowski1989@gmail.com>
2018-02-11 22:30:27 +0100

    Sass integration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/main';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
